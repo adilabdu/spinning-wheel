b9c2df72a391586f8c3d137f9cf8ea3d7c770675
Adil Abdu Bushra <adilabdu68@gmail.com>
2022-01-04 23:56:37 +0900

    Elements rendered, Spinning Wheel now visible

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            greetings: "Hello Vue!"
+            greetings: "Hello Vue!",
+            innerLabels: [6, 12, 19, 25, 31, 38],
+            labels: {
+                6: "B",
+                12: "C",
+                19: "D",
+                25: "E",
+                31: "F",
+                38: "A"
+            }
+        }
+    },
+    methods: {
+        showLabel(index) {
+            return this.innerLabels.includes(index)
         }
     }
 })
